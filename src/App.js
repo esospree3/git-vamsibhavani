47c7d4b9891b0ee9f151bc770e4c4128f6dd1775
Eswara Prasad <esospree333@gmail.com>
2025-05-31 18:18:10 -0400

    commmitting all the changed files  those are src/App.js package.jsonpackage-lock.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Page1 from "./components/Page1";
+import Page2 from "./components/Page2";
+import { Box, Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Box sx={{ p: 5, minHeight: "2000vh", bgcolor: "background.default" }}>
+        <Typography variant="h1" align="center" gutterBottom>
+          Navigation App
+        </Typography>
+        <Routes>
+          <Route path="/" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+        </Routes>
+      </Box>
+    </Router>
   );
 }
 
